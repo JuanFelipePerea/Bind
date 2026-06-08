20630de0c2254efd514e4a6443bcbfe93220aaff
John Lark <pipecuaran09@gmail.com>
2026-06-08 13:10:03 -0500

    no more emojis pls + Gmail V.23

--- BindV1/settings.py
+++ BindV1/settings.py
@@ -169,6 +169,7 @@ EMAIL_BACKEND = os.environ.get(
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = f'BIND <{os.environ.get("EMAIL_HOST_USER", "noreply@bind.local")}>'
